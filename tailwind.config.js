177ebe02a573524391464f9b7def30a556c7b8cb
Matt Dunbar <mattdunbar99@gmail.com>
2023-08-14 02:07:54 +0700

    Full code quality test suite passing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -21,4 +21,4 @@ export default {
     },
 
     plugins: [forms, typography],
-};
+}
