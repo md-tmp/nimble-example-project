d09fc2456665247b5d10d82c8a39c04476d1d92b
Matt Dunbar <mattdunbar99@gmail.com>
2023-08-16 01:15:04 +0700

    Added API Support

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\KeywordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get(
     '/user', function (Request $request) {
         return $request->user();
+    }
+);
+
+Route::group(
+    [
+        'prefix' => 'v1',
+        'namespace' => 'App\Http\Controllers\Api\V1',
+        'middleware' => ['auth:sanctum']
+    ],
+    function () {
+        Route::apiResource('keywords', KeywordController::class)->only(['index', 'show', 'store']);
     }
 );
